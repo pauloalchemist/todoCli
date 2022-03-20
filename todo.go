4a803d0b27764b9823f08e18297abe1792985678
pauloalchemist <paulolinsdev@gmail.com>
2022-03-20 13:32:00 -0300

    added get method

--- todo.go
+++ todo.go
@@ -2,6 +2,7 @@ package todo
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"time"
@@ -57,4 +58,20 @@ func (l *List) Save(filename string) error {
 	}
 
 	return os.WriteFile(filename, js, 0644)
+}
+
+func (l *List) Get(filename string) error {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+
+	if len(file) == 0 {
+		return nil
+	}
+
+	return json.Unmarshal(file, l)
 }
