be1f04d9b07b0f087cda66a3e1a23ebd453dbb3d
pauloalchemist <paulolinsdev@gmail.com>
2022-03-25 22:55:01 -0300

    tests for todoCli

--- cmd/todo/main_test.go
+++ cmd/todo/main_test.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"runtime"
+	"strings"
 	"testing"
 )
 
@@ -34,4 +36,37 @@ func TestMain(m *testing.M) {
 	os.Remove(fileName)
 
 	os.Exit(result)
+}
+
+func TestTodoCLI(t *testing.T) {
+	task := "test task number 1"
+
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cmdPath := filepath.Join(dir, binName)
+
+	t.Run("AddNewTask", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, strings.Split(task, " ")...)
+
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("ListTasks", func(t *testing.T) {
+		cmd := exec.Command(cmdPath)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		expected := task + "\n"
+
+		if expected != string(out) {
+			t.Errorf("Expected %q, got %q instead\n", expected, string(out))
+		}
+	})
 }
