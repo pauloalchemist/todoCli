7e6ff3dcfd9c29e6720a0d949d37664758d7df67
pauloalchemist <paulolinsdev@gmail.com>
2022-03-25 22:36:56 -0300

    added main test

---
+++ cmd/todo/main_test.go
@@ -0,0 +1,37 @@
+package main_test
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"testing"
+)
+
+var (
+	binName  = "todo"
+	fileName = ".todo.json"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("Building tool...")
+	if runtime.GOOS == "windows" {
+		binName += ".exe"
+	}
+
+	build := exec.Command("go", "build", "-o", binName)
+
+	if err := build.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot build tool %s: %s", binName, err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Running tests...")
+	result := m.Run()
+
+	fmt.Println("Cleaning up...")
+	os.Remove(binName)
+	os.Remove(fileName)
+
+	os.Exit(result)
+}
