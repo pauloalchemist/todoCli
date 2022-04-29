e677bdb5013d8c94dc2e8f7eb8baf7ce20bb9c20
pauloalchemist <paulolinsdev@gmail.com>
2022-04-28 23:54:08 -0300

    added tags in cli

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	todo "go-cap2/todoCli"
 	"os"
-	"strings"
 )
 
 const todoFileName = ".todo.json"
 
 func main() {
+	task := flag.String("task", "", "Tarefa a ser inclusa no ToDo List")
+	list := flag.Bool("list", false, "Listar todas as tarefas")
+	complete := flag.Int("complete", 0, "Alterar tarefa para concluida")
+
+	flag.Parse()
+
 	l := &todo.List{}
 
 	if err := l.Get(todoFileName); err != nil {
@@ -18,16 +24,38 @@ func main() {
 	}
 
 	switch {
-	case len(os.Args) == 1:
+	case *list: // lista a tarefas a fazer
 		for _, item := range *l {
-			fmt.Println(item.Task)
+			if !item.Done {
+				fmt.Println(item.Task)
+			}
 		}
-	default:
-		item := strings.Join(os.Args[1:], " ")
-		l.Add(item)
+
+	case *complete > 0:
+		// ↓ altera tarefa pata finalizada (complete)
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		// ↓ salva no item na lista de tarefas
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+
+	case *task != "":
+		// ↓ adiciona nova tarefa
+		l.Add(*task)
+
+		// ↓ salva na lista
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+	default:
+		// ↓ se uma flag inválida for passada
+		fmt.Fprintln(os.Stderr, "Opção inválida. Tente novamente.")
+		os.Exit(1)
 	}
 }
